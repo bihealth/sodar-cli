ce542e75c6f5297a8b77bb63e9d5b5857900f763
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-01-10 13:43:24 +0100

    fix lints

--- tests/test_samplesheet.py
+++ tests/test_samplesheet.py
@@ -168,7 +168,7 @@ def test_samplesheet_export(capsys, requests_mock, tmpdir):
     captured = capsys.readouterr()
 
     assert json.dumps(result) + "\n" == captured.out
-    assert list(sorted(x.basename for x in tmpdir.listdir())) == [
+    assert sorted(x.basename for x in tmpdir.listdir()) == [
         "a_test_exome_sequencing_nucleotide_sequencing.txt",
         "i_Investigation.txt",
         "s_test.txt",
